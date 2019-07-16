4b645d258cc5e2415daf7a9b219c636a9d3922e4
misato <trbnpos5@outlook.jp>
2019-07-16 19:45:42 +0900

    ideasコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  resources :ideas, only: [:index, :show, :new, :create]
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  
   root to: 'dynamic_pages#top'
 end
