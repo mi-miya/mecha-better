3e8831ae984b2012611adbf0f1ebf8490594bef9
misato <trbnpos5@outlook.jp>
2019-07-16 21:46:56 +0900

    idea投稿機能実装

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,32 @@
 class IdeasController < ApplicationController
+  before_action :authenticate
+
   def index
+    @ideas = Idea.all
   end
 
   def new
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+    @idea.state = "未実現"
+    @idea.user = current_user
+    if @idea.save
+      redirect_to @idea
+    else
+      render ideas_path
+    end
   end
 
   def show
+    @idea = Idea.find(params[:id])
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body, :user_id, :state, :tag)
   end
 end
