17483768ce7df6c922a6bd139e4f89a82263db1d
misato <trbnpos5@outlook.jp>
2019-07-17 16:06:21 +0900

    拍手機能実装

---
+++ app/controllers/applauses_controller.rb
@@ -0,0 +1,16 @@
+class ApplausesController < ApplicationController
+    def create
+        @idea = Idea.find(params[:idea_id])
+        current_user.applaud(@idea)
+        respond_to do |format|
+          format.html { redirect_to @idea }
+          format.js
+        end
+    end
+
+    def destroy
+        @idea = Idea.find(params[:idea_id])
+        current_user.un_applaud(@idea)
+        redirect_to ideas_path
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :ideas, only: [:index, :show, :new, :create] do
     resources :idea_reactions, :only => [:create]
   end
+  resources :applauses, only: [:create, :destroy]
   get 'auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
   root to: 'dynamic_pages#top'
